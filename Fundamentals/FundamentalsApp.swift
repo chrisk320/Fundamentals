4facdb8aa6dffdadea6956ddb73541b15884731b
Christian Kim <chris.kim320@gmail.com>
2025-02-03 12:46:59 -0500

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Christian Kim on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
